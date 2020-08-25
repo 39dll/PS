4c6fca0c001a6fcc528c48e879710b5e559b5e92
39dll <37054037+39dll@users.noreply.github.com>
2020-08-25 09:02:04 +0900

    Create A.cpp

---
+++ Codeforces/Codeforces Round #663/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int T;
+	cin >> T;
+	while (T--) {
+		int N;
+		cin >> N;
+		for (int i = 1; i <= N; i++)cout << i << ' ';
+		cout << '\n';
+	}
+}
