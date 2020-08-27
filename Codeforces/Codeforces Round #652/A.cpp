83ce67708dcf35304797544455e6569e7821eef3
39dll <37054037+39dll@users.noreply.github.com>
2020-08-28 06:51:03 +0900

    Create A.cpp

---
+++ Codeforces/Codeforces Round #652/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		int N;
+		cin >> N;
+		if (N % 4 == 0)cout << "YES\n";
+		else cout << "NO\n";
+	}
+}
