860b6f7808f52566f3f079128dfc3d2a96247603
39dll <37054037+39dll@users.noreply.github.com>
2020-09-11 22:39:35 +0900

    Update Power.cpp

--- Template/Power.cpp
+++ Template/Power.cpp
@@ -1,7 +1,4 @@
-﻿#include <iostream>
-using namespace std;
-long long A, B, C, res = 0;
-long long calc(long long A, long long B) {
+long long calc(long long A, long long B, long long C) {
 	if (B == 1) { return (A % C); }
 	long long temp = calc(A, B / 2) % C;
 	if (B % 2 == 0) {
@@ -10,9 +7,4 @@ long long calc(long long A, long long B) {
 	else {
 		return ((temp * temp) % C * (calc(A, 1))) % C;
 	}
-}
-int main() { //A의 B제곱을 C로 나눈 값 출력
-	cin >> A >> B >> C;
-	res = calc(A, B);
-	cout << res;
 }
