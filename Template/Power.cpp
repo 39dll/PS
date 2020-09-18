cdccefbc2fa577bd655d4034dcd77d3019c166f5
39dll <37054037+39dll@users.noreply.github.com>
2020-09-18 22:55:04 +0900

    Update Power.cpp

--- Template/Power.cpp
+++ Template/Power.cpp
@@ -1,10 +1,7 @@
 long long mypow(long long A, long long B, long long C) {
-	if (B == 1) { return (A % C); }
+	if (B == 0)return 1;
+	if (B == 1)return (A % C);
 	long long temp = mypow(A, B / 2, C) % C;
-	if (B % 2 == 0) {
-		return temp * temp % C;
-	}
-	else {
-		return ((temp * temp) % C * (mypow(A, 1, C))) % C;
-	}
+	if (B % 2 == 0)return temp * temp % C;
+	return ((temp * temp) % C * (mypow(A, 1, C))) % C;
 }
