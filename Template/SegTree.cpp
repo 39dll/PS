52a44fa74bcd48f9b4bebf50ed1f2c887ea82b92
39dll <37054037+39dll@users.noreply.github.com>
2020-09-30 10:49:05 +0900

    Update SegTree.cpp

--- Template/SegTree.cpp
+++ Template/SegTree.cpp
@@ -1,12 +1,22 @@
-﻿#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-const long long INF = 9223372036854775807;
 const long long MAX_SIZE = 1000001;
 long long tree[MAX_SIZE * 4]; //tree[N]의 자식은 tree[2*N], tree[2*N+1]
 long long N, M, depth, st;
 
+void update(long long ind, long long val) {
+	tree[st + ind] = val;
+	ind = (ind + 1) / 2;
+	for (int i = depth - 1; i >= 1; i--) {
+		int cur = pow(2, i - 1) - 1 + ind;
+		tree[cur] = tree[cur * 2] + tree[cur * 2 + 1];
+		ind = (ind + 1) / 2;
+	}
+}
+
 long long query(long long start, long long end) {
 	long long ret = 0;
+	start += st; end += st;
 	while (start <= end) {
 		if (start % 2 == 1) ret += tree[start];
 		if (end % 2 == 0) ret += tree[end];
@@ -22,7 +32,7 @@ int main() {
 	//초기화
 	//for (int i = 0; i < MAX_SIZE * 3; i++)tree[i] = 0;
 
-	//수의 개수, 쿼리의 개수
+	//수의 개수
 	cin >> N;
 
 	//전처리
@@ -31,7 +41,7 @@ int main() {
 		if (N <= (long long)pow(2, depth - 1))break;
 		depth++;
 	}
-	st = (long long)pow(2, depth - 1) - 1; //tree[i + st]에 실제 값들이 있음
+	st = (long long)pow(2, depth - 1) - 1;
 
 	//숫자 입력
 	for (int i = 1; i <= N; i++) {
@@ -45,7 +55,8 @@ int main() {
 			tree[ind] = tree[ind * 2] + tree[ind * 2 + 1];
 		}
 	}
-	
+
+	//쿼리의 개수
 	cin >> M;
 
 	//쿼리 처리
@@ -54,16 +65,10 @@ int main() {
 		cin >> in1 >> in2 >> in3;
 
 		if (in1 == 1) {//set in2-nd index to in3
-			tree[st + in2] = in3;
-			in2 = (in2 + 1) / 2;
-			for (int i = depth - 1; i >= 1; i--) {
-				int ind = pow(2, i - 1) - 1 + in2;
-				tree[ind] = tree[ind * 2] + tree[ind * 2 + 1];
-				in2 = (in2 + 1) / 2;
-			}
+			update(in2, in3);
 		}
 		else if (in1 == 2) {//perform query for in2 ~ in3
-			cout << query(st + in2, st + in3) << '\n';
+			cout << query(in2, in3) << '\n';
 		}
 	}
 }
