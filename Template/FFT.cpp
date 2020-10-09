532e3dfe3cf336aa21d176db8a2e10afd59adf8d
39dll <37054037+39dll@users.noreply.github.com>
2020-10-09 23:47:20 +0900

    Update FFT.cpp

--- Template/FFT.cpp
+++ Template/FFT.cpp
@@ -1,88 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef complex<long double> cpx;
-typedef vector<cpx> vec;
 const long double pi = acos(-1);
+typedef complex<double> base;
 
-void FFT(vec &f, cpx w) {
-	int n = f.size();
-	if (n == 1) return;
-	vec even(n >> 1), odd(n >> 1);
-	for (int i = 0; i < n; i++) {
-		if (i & 1) odd[i >> 1] = f[i];
-		else even[i >> 1] = f[i];
+void fft(vector <base> &a, bool invert) {
+	int n = a.size();
+	for (int i = 1, j = 0; i<n; i++) {
+		int bit = n >> 1;
+		for (; j >= bit; bit >>= 1) j -= bit;
+		j += bit;
+		if (i < j) swap(a[i], a[j]);
 	}
-	FFT(even, w*w); FFT(odd, w*w);
-	cpx wp(1, 0);
-	for (int i = 0; i < n / 2; i++) {
-		f[i] = even[i] + wp * odd[i];
-		f[i + n / 2] = even[i] - wp * odd[i];
-		wp = wp * w;
+	for (int len = 2; len <= n; len <<= 1) {
+		double ang = 2 * pi / len * (invert ? -1 : 1);
+		base wlen(cos(ang), sin(ang));
+		for (int i = 0; i<n; i += len) {
+			base w(1);
+			for (int j = 0; j<len / 2; j++) {
+				base u = a[i + j], v = a[i + j + len / 2] * w;
+				a[i + j] = u + v;
+				a[i + j + len / 2] = u - v;
+				w *= wlen;
+			}
+		}
 	}
+	if (invert)
+		for (int i = 0; i<n; i++) a[i] /= n;
 }
 
-/*
-input : vector a, b
-output : vector containing convolusion of a and b
-size of res : a.size() + b.size() - 1
-*/
-vec mul(vec a, vec b) {
-	long long n = 1;
-
-	while (n <= a.size() || n <= b.size()) n <<= 1;
+void mul(const vector<int> &a, const vector<int> &b, vector<int> &res) {
+	vector<base> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+	int n = 1;
+	while (n < max(a.size(), b.size())) n <<= 1;
 	n <<= 1;
-	a.resize(n); b.resize(n); vec c(n);
-
-	//principal n-th root of unity
-	cpx w(cos(2 * pi / n), sin(2 * pi / n));
-
-	//a와 b의 DFT구하기
-	FFT(a, w); FFT(b, w);
-
-	//f(x) * g(x) = h(x)
-	for (int i = 0; i < n; i++) c[i] = a[i] * b[i];
-
-	//켤레 복소수로 DFT -> IDFT
-	FFT(c, cpx(w.real(), -w.imag()));
-	for (int i = 0; i < n; i++) {
-		c[i] /= cpx(n, 0);
-
-		//만약 정수 결과를 원한다면
-		//c[i] = cpx(round(c[i].real()), round(c[i].imag())); 
-	}
-	return c;
+	fa.resize(n); fb.resize(n);
+	fft(fa, false); fft(fb, false);
+	for (int i = 0; i < n; i++) fa[i] *= fb[i];
+	fft(fa, true);
+	res.resize(n);
+	for (int i = 0; i < n; i++) res[i] = int(fa[i].real() + 0.5);
 }
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	string a, b;
-	cin >> a >> b;
-	if (a == "0" || b == "0") {
-		cout << 0;
-		return 0;
+	int N, M;
+	vector<int> v1, v2, res;
+	cin >> N >> M;
+	for (int i = 0; i < N; i++) {
+		int in; cin >> in;
+		v1.push_back(in);
 	}
-	int sz = a.size() + b.size() - 1;
-	vec v1, v2;
-	for (int i = 0; i < a.length(); i++) {
-		v1.push_back(a[i] - '0');
+	for (int i = 0; i < M; i++) {
+		int in; cin >> in;
+		v2.push_back(in);
 	}
-	for (int i = 0; i < b.length(); i++) {
-		v2.push_back(b[i] - '0');
-	}
-	vec res;
-	res = mul(v1, v2);
-	vector<int> ans(sz + 1);
-	for (int i = sz - 1; i >= 0; i--) {
-		ans[i] += res[i].real() / 10;
-		ans[i + 1] += (int)res[i].real() % 10;
-		if (ans[i + 1] >= 10) {
-			ans[i] += ans[i + 1] / 10;
-			ans[i + 1] %= 10;
-		}
-	}
-	for (int i = 0; i < sz + 1; i++) {
-		if (i == 0 && ans[0] == 0)continue;
-		cout << ans[i];
+	mul(v1, v2, res);
+	for (int i = 0; i < N + M - 1; i++) {
+		cout << res[i] << ' ';
 	}
 }
