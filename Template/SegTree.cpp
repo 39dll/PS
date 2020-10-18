fb2e50eed7ec7083933655cc18f6839c868e52fb
39dll <37054037+39dll@users.noreply.github.com>
2020-10-18 23:04:07 +0900

    Update SegTree.cpp

--- Template/SegTree.cpp
+++ Template/SegTree.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 const long long MAX_SIZE = 1000001;
-long long tree[MAX_SIZE * 4]; //tree[N]의 자식은 tree[2*N], tree[2*N+1]
+long long tree[MAX_SIZE * 4];
 long long N, M, depth, st;
 
 void update(long long ind, long long val) {
@@ -29,13 +29,8 @@ long long query(long long start, long long end) {
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
-	//초기화
-	//for (int i = 0; i < MAX_SIZE * 4; i++)tree[i] = 0;
-
-	//수의 개수
 	cin >> N;
 
-	//전처리
 	depth = 1;
 	while (1) {
 		if (N <= (long long)pow(2, depth - 1))break;
@@ -43,12 +38,10 @@ int main() {
 	}
 	st = (long long)pow(2, depth - 1) - 1;
 
-	//숫자 입력
 	for (int i = 1; i <= N; i++) {
 		cin >> tree[i + st];
 	}
 
-	//세그먼트 트리 갱신
 	for (int i = depth - 1; i >= 1; i--) {
 		for (int j = 0; j < pow(2, i - 1); j++) {
 			long long ind = pow(2, i - 1) + j;
@@ -56,10 +49,8 @@ int main() {
 		}
 	}
 
-	//쿼리의 개수
 	cin >> M;
 
-	//쿼리 처리
 	for (int t = 0; t < M; t++) {
 		int in1, in2, in3;
 		cin >> in1 >> in2 >> in3;
