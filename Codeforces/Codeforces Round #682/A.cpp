5f1507a5c3950f3aa99525af68db140b0ac72de1
39dll <37054037+39dll@users.noreply.github.com>
2020-11-18 22:48:41 +0900

    Create A.cpp

---
+++ Codeforces/Codeforces Round #682/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int T; cin >> T;
+	while (T--) {
+		int N; cin >> N;
+		while (N--)cout << "1 ";
+		cout << '\n';
+	}
+}
