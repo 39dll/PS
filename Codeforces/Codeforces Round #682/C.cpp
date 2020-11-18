a3ce1c6f49b6b9d40d6e863164dec8f4d9065da0
39dll <37054037+39dll@users.noreply.github.com>
2020-11-18 22:49:13 +0900

    Create C.cpp

---
+++ Codeforces/Codeforces Round #682/C.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dx[] = { -1,1,0,0 }, dy[] = { 0,0,-1,1 };
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int T; cin >> T;
+	while (T--) {
+		int N, M; cin >> N >> M;
+		int arr[101][101];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				cin >> arr[i][j];
+				if ((i + j) % 2 == 0) {
+					if (arr[i][j] % 2 == 0) {
+						arr[i][j]++;
+					}
+				}
+				else {
+					if (arr[i][j] % 2 == 1) {
+						arr[i][j]++;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				cout << arr[i][j] << ' ';
+			}cout << '\n';
+		}
+	}
+}
