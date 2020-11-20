27dc1ba03e88b75688a2f23d7f22584ba6b739df
39dll <37054037+39dll@users.noreply.github.com>
2020-11-20 21:15:05 +0900

    Create B.cpp

---
+++ Codeforces/Codeforces Round #684/B.cpp
@@ -0,0 +1,17 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int T; cin >> T; while (T--) {
+
+		int N, K; cin >> N >> K;
+
+		vector<long long> v(N*K);
+		for (int i = 0; i < N*K; i++)cin >> v[i];
+		long long res = 0;
+		for (int k = K*((N - 1) / 2); k < N*K; k += N - (N - 1) / 2) {
+			res += v[k];
+		}
+		cout << res << '\n';
+	}
+}
