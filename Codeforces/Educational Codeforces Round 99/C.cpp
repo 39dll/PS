6698085963042982d24d313db6f5063a258d660d
39dll <37054037+39dll@users.noreply.github.com>
2020-12-01 20:56:16 +0900

    Create C.cpp

---
+++ Codeforces/Educational Codeforces Round 99/C.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int T; cin >> T; while (T--) {
+		long long x, y;
+		cin >> x >> y;
+		cout << x - 1 << ' ' << y << '\n';
+	}
+}
