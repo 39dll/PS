76fb14c8fd8eecfab9c98ea1b7f52ffd5bbbb8cd
39dll <37054037+39dll@users.noreply.github.com>
2020-12-11 12:26:24 +0900

    Update Dijkstra.cpp

--- Template/Dijkstra.cpp
+++ Template/Dijkstra.cpp
@@ -1,46 +1,22 @@
-﻿#include <bits/stdc++.h>
-using namespace std;
-typedef pair<long long, int> p; //가중치, 도착점
+typedef pair<long long, int> p;
 const long long INF = 10000000000000;
-int V, E, K;
-long long v[20001];
-vector<p> e[20001];
-void init() {
-	for (int i = 1; i <= V; i++) {
-		v[i] = INF;
-	}
-}
-void dijkstra(int start) {
-	init();
-	priority_queue<p, vector<p>, greater<p> > pq;
+vector<long long> dijkstra(int start) {
+	vector<long long> v(N + 1);
+	for (auto &i : v)i = INF;
+	priority_queue<p, vector<p>, greater<p>> pq;
 	v[start] = 0;
 	pq.push({ 0, start });
 	while (!pq.empty()) {
-		long long cost = pq.top().first;
-		int des = pq.top().second;
-		pq.pop();
-		if (v[des] < cost) continue;
-		for (int i = 0; i < e[des].size(); i++) {
-			int there = e[des][i].second;
-			long long next = cost + e[des][i].first;
+		long long cost, des;
+		tie(cost, des) = pq.top(); pq.pop();
+		for (auto k : e[des]) {
+			long long next, there;
+			tie(next, there) = k; next += cost;
 			if (v[there] > next) {
 				v[there] = next;
 				pq.push({ next,there });
 			}
 		}
 	}
-}
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	cin >> V >> E >> K;
-	for (int i = 1; i <= E; i++) {
-		long long in1, in2, in3;
-		cin >> in1 >> in2 >> in3;
-		e[in1].push_back({ in3, in2 });
-	}
-	dijkstra(K);
-	for (int i = 1; i <= V; i++) {
-		if (v[i] == INF) cout << "INF\n";
-		else cout << v[i] << "\n";
-	}
+	return v;
 }
