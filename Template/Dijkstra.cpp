384cb42185a68e855b6b85aca2abeabde1fc66a6
39dll <37054037+39dll@users.noreply.github.com>
2020-12-11 12:28:51 +0900

    Update Dijkstra.cpp

--- Template/Dijkstra.cpp
+++ Template/Dijkstra.cpp
@@ -4,8 +4,7 @@ vector<long long> dijkstra(int start) {
 	vector<long long> v(N + 1);
 	for (auto &i : v)i = INF;
 	priority_queue<p, vector<p>, greater<p>> pq;
-	v[start] = 0;
-	pq.push({ 0, start });
+	v[start] = 0; pq.push({ 0, start });
 	while (!pq.empty()) {
 		long long cost, des, next, there;
 		tie(cost, des) = pq.top(); pq.pop();
