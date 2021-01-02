164d9d7aa0ecaad1e68220fadbcf008b8045bb67
39dll <37054037+39dll@users.noreply.github.com>
2021-01-03 01:51:00 +0900

    Update G++.cpp

--- Template/G++.cpp
+++ Template/G++.cpp
@@ -3,17 +3,15 @@
 #include <unordered_map>
 #include <ext/rope>
 #pragma GCC optimize("O3")
-#define DEBUG 0
+#define DEBUG 1
 #define dout if(DEBUG) cout
+#define fuck if(DEBUG)cout.flush(),system("pause"); return 0
 using namespace std;
 using namespace __gnu_cxx;
+typedef long long ll;
+typedef pair<ll, ll> pll;
 int main(){
     if(!DEBUG)ios::sync_with_stdio(0), cin.tie(0);
     
-    clock_t clockst = clock();
-
-    //main
-
-    dout << setprecision(3) << fixed << "\nRuntime : " << (clock() - clockst) / double(CLOCKS_PER_SEC) << "sec\n";
-    if (DEBUG)cout.flush(), system("pause");
+    fuck;
 }
