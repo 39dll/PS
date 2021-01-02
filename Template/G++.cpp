a2be9f2dac02939fa85208e9bc8f107a93fc29f6
39dll <37054037+39dll@users.noreply.github.com>
2021-01-02 22:48:48 +0900

    Create G++.cpp

---
+++ Template/G++.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <unordered_map>
+#include <ext/rope>
+#pragma GCC optimize("O3")
+#define DEBUG 0
+#define dout if(DEBUG) cout
+using namespace std;
+using namespace __gnu_cxx;
+int main(){
+    if(!DEBUG)ios::sync_with_stdio(0), cin.tie(0);
+    
+    clock_t clockst = clock();
+
+    //main
+
+    dout << setprecision(3) << fixed << "\nRuntime : " << (clock() - clockst) / double(CLOCKS_PER_SEC) << "sec\n";
+    cout.flush();
+    if (DEBUG)system("pause");
+}
