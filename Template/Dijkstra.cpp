4ac7ac76ac3e8dd58c844b787f3e9c9245a7868d
39dll <37054037+39dll@users.noreply.github.com>
2021-05-06 11:08:25 +0900

    Update Dijkstra.cpp

--- Template/Dijkstra.cpp
+++ Template/Dijkstra.cpp
@@ -1,20 +1,20 @@
 typedef pair<long long, int> p;
-const long long INF = 10000000000000;
-vector<long long> dijkstra(int start) {
-	vector<long long> v(N + 1);
-	for (auto &i : v)i = INF;
-	priority_queue<p, vector<p>, greater<p>> pq;
-	v[start] = 0; pq.push({ 0, start });
-	while (!pq.empty()) {
-		long long cost, des, next, there;
-		tie(cost, des) = pq.top(); pq.pop();
-		for (auto k : e[des]) {
-			tie(next, there) = k; next += cost;
-			if (v[there] > next) {
-				v[there] = next;
-				pq.push({ next,there });
+
+auto dijkstra = [&](int start, vector<p> e[]) -> vector<ll>{
+		vector<long long> v(N + 1);
+		for (auto &i : v)i = INF;
+		priority_queue<p, vector<p>, greater<p>> pq;
+		v[start] = 0; pq.push({ 0, start });
+		while (!pq.empty()) {
+			long long cost, des, next, there;
+			tie(cost, des) = pq.top(); pq.pop();
+			for (auto k : e[des]) {
+				tie(next, there) = k; next += cost;
+				if (v[there] > next) {
+					v[there] = next;
+					pq.push({ next,there });
+				}
 			}
 		}
-	}
-	return v;
-}
+		return v;
+	};
