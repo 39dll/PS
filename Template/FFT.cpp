2011f0306ed8adcb53389e5fcb2a9972d2228434
39dll <37054037+39dll@users.noreply.github.com>
2021-07-21 00:34:36 +0900

    Update FFT.cpp

--- Template/FFT.cpp
+++ Template/FFT.cpp
@@ -1,64 +1,54 @@
-#include <bits/stdc++.h>
-using namespace std;
+using ll = long long;
+using ld = double;
+using base = complex<ld>;
+const ld pi = acos(-1);
 
-const long double pi = acos(-1);
-typedef complex<double> base;
-
-void fft(vector <base> &a, bool invert) {
-	int n = a.size();
-	for (int i = 1, j = 0; i<n; i++) {
-		int bit = n >> 1;
-		for (; j >= bit; bit >>= 1) j -= bit;
-		j += bit;
-		if (i < j) swap(a[i], a[j]);
+void fft(vector <base> &A, bool f) {
+	int k = A.size(), i, j, l, t;
+	base w, x, y; ld th;
+	for (i = 1, j = 0; i < k; i++) {
+		for (l = k >> 1; j >= l; l >>= 1) j -= l;
+		j += l; if(i < j) swap(A[i], A[j]);
 	}
-	for (int len = 2; len <= n; len <<= 1) {
-		double ang = 2 * pi / len * (invert ? -1 : 1);
-		base wlen(cos(ang), sin(ang));
-		for (int i = 0; i<n; i += len) {
-			base w(1);
-			for (int j = 0; j<len / 2; j++) {
-				base u = a[i + j], v = a[i + j + len / 2] * w;
-				a[i + j] = u + v;
-				a[i + j + len / 2] = u - v;
-				w *= wlen;
+	for (i = 1; i < k; i <<= 1, t--) {
+		th = (f ? -pi : pi) / i;
+		w = base(cos(th), sin(th));
+		for (j = 0; j < k; j += i + i) {
+			for (l = 0; l < i; l++) {
+				if(l & 2047) x *= w;
+				else x = l ? base(cos(th * l), sin(th * l)) : 1;
+				y = x * A[l | i | j];
+				A[l | i | j] = A[l | j] - y;
+				A[l | j] += y;
 			}
 		}
 	}
-	if (invert)
-		for (int i = 0; i<n; i++) a[i] /= n;
+	if (f) for (i = 0; i < k; i++) {
+		A[i] /= k;
+	}
 }
 
-vector<int> mul(const vector<int> &a, const vector<int> &b) {
-	vector<base> fa(a.begin(), a.end()), fb(b.begin(), b.end());
-	int n = 1;
-	while (n < max(a.size(), b.size())) n <<= 1;
-	n <<= 1;
-	fa.resize(n); fb.resize(n);
-	fft(fa, false); fft(fb, false);
-	for (int i = 0; i < n; i++) fa[i] *= fb[i];
-	fft(fa, true);
-	vector<int> res;
-	res.resize(n);
-	for (int i = 0; i < n; i++) res[i] = int(fa[i].real() + 0.5);
-	return res;
-}
-
-int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int N, M;
-	vector<int> v1, v2, res;
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		int in; cin >> in;
-		v1.push_back(in);
+vector<ll> mult(vector<ll> &X, vector<ll> &Y) { //return vector's size is |X| + |Y| - 1
+	ll s, i, j;
+	for (s = 1; s < X.size() + Y.size(); s <<= 1);
+	vector<base> P(s), Q(s);
+	vector<ll> Z(X.size() + Y.size() - 1);
+	for (i = 0; i < X.size(); i++)
+		P[i] = base(X[i] >> 12, X[i] & 4095);
+	for (i = 0; i < Y.size(); i++)
+		Q[i] = base(Y[i] >> 12, Y[i] & 4095);
+	fft(P, 0); fft(Q, 0);
+	for (i = 0; i + i <= s; i++) {
+		j = i ? s - i : 0;
+		base v1 = P[i] + conj(P[j]), v2 = conj(P[i]) - P[j];
+		tie(P[i], Q[i], P[j], Q[j]) = make_tuple(
+			v1 * Q[i], conj(v2) * conj(Q[j]),
+			conj(v1) * Q[j], -v2 * conj(Q[i]));
 	}
-	for (int i = 0; i < M; i++) {
-		int in; cin >> in;
-		v2.push_back(in);
+	fft(P, 1); fft(Q, 1);
+	for(i = 0; i < Z.size(); i++) {
+		Z[i] = (((ll)round(P[i].real()) << 23) + ((ll)round(Q[i].real()) >> 1)
+			+ ((ll)round(P[i].imag() + Q[i].imag()) << 11));
 	}
-	vector<int> res = mul(v1, v2);
-	for (int i = 0; i < N + M - 1; i++) {
-		cout << res[i] << ' ';
-	}
+	return Z;
 }
